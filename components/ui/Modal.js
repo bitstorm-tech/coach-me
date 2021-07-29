b7d6578cb34eb73aae4b4c9c9e03a0ffee768472
Josef Bauer <josef.bauer.1st@gmail.com>
2021-07-29 22:51:00 +0200

    Extract CloseCross component

--- components/ui/Modal.js
+++ components/ui/Modal.js
@@ -1,4 +1,5 @@
 import PrimaryButton from './buttons/PrimaryButton';
+import CloseCross from './CloseCross';
 
 export default function Modal({ children, visible, onClose, onSave, title }) {
   if (!visible) {
@@ -10,21 +11,7 @@ export default function Modal({ children, visible, onClose, onSave, title }) {
       <div className="flex flex-col mx-auto w-11/12 max-w-2xl rounded-md border border-accent sm:w-5/6 lg:w-1/2">
         <div className="flex flex-row justify-between p-6 text-primary bg-darkest rounded-t-md">
           <p className="font-bold">{title}</p>
-          <svg
-            className="w-6 h-6 cursor-pointer hover:text-accent hover-transition"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-            onClick={onClose}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
+          <CloseCross onClick={onClose} />
         </div>
         <div className="p-5 bg-darker">{children}</div>
         <div className="flex flex-row justify-between items-center p-5 bg-darkest rounded-b-md">
